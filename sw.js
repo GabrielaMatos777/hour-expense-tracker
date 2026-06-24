ad0f4dcc7d4645690ebd4a9711238bd1b5ec119d
Gabriela Matos <gabyafmatos@gmail.com>
2026-06-24 22:32:32 +0100

    feat: Add PWA support for mobile installation (manifest, service worker, icons)

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'hourflow-v2';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css?v=2',
+  '/app.js?v=2',
+  '/manifest.json',
+  '/icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
